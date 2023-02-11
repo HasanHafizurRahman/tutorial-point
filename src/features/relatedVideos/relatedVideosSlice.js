fe07098f619908095297d7f08e539a982bc30aa3
Hasan Hafizur Rahman <hasanshanto922@gmail.com>
2023-02-11 17:24:56 +0600

    relatedvideoslice

--- src/features/relatedVideos/relatedVideosSlice.js
+++ src/features/relatedVideos/relatedVideosSlice.js
@@ -1,12 +1,41 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { getRelatedVideo } from "./relatedVideosApi";
 
-const initialState = {};
+const initialState = {
+  relatedVideos: [],
+  isLoading: false,
+  isError: false,
+  error: "'",
+};
 
 export const fetchRelatedVideo = createAsyncThunk(
-  "relatedVideo/fetchRelatedVideo",
-  async (id, tags) => {
-    const relatedVideo = getRelatedVideo();
-    return relatedVideo;
+  "relatedVideos/fetchRelatedVideo",
+  async ({ id, tags }) => {
+    const relatedVideos = getRelatedVideo({ id, tags });
+    return relatedVideos;
   }
 );
+
+const relatedVideosSlice = createSlice({
+  name: "relatedVideos",
+  initialState,
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchRelatedVideo.pending, (state) => {
+        state.isError = false;
+        state.isLoading = true;
+      })
+      .addCase(fetchRelatedVideo.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.relatedVideos = action.payload;
+      })
+      .addCase(fetchRelatedVideo.rejected, (state, action) => {
+        state.isLoading = false;
+        state.relatedVideos = {};
+        state.isError = true;
+        state.error = action.error?.message;
+      });
+  },
+});
+
+export default relatedVideosSlice.reducer;

5d63e500f2a3e4613d7d7a19a2b48eac6b2d52e6
Hasan Hafizur Rahman <hasanshanto922@gmail.com>
2023-02-13 23:31:40 +0600

    loading spinner

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,7 +1,17 @@
 import React from "react";
 
 const Loading = () => {
-  return <div>Loading...</div>;
+  return (
+    <div class="h-screen bg-white">
+      <div class="flex justify-center h-full">
+        <img
+          class="h-16 w-16"
+          src="https://icons8.com/preloaders/preloaders/1488/Iphone-spinner-2.gif"
+          alt=""
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Loading;
